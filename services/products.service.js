ea1bdd60e78e60134875c4adc65e809c0bfd89b9
bisho <bishoymelekwadie@gmail.com>
2018-11-13 09:15:06 +0200

    Docs/added API docs for products service

--- services/products.service.js
+++ services/products.service.js
@@ -6,20 +6,34 @@ module.exports = {
 	mixins: [ESService],
 	dependencies: [],
 	actions: {
+		/**
+		 * @api {post} /prodcuts/create Add(index) new product
+		 * @apiName create products
+		 * @apiGroup products
+		 *
+		 * @apiParam (newProduct) {Object} newProduct new product object to index.
+		 * @apiParam (newProduct) {String} newProduct.index product index.
+		 * @apiParam (newProduct) {String} newProduct.type  product type.
+		 * @apiParam (newProduct) {String} newProduct.title product title.
+		 *
+		 * @apiSuccess {Boolean} success ensuring indexing
+		 * @apiError {Boolean}  success ensuring failure to index.
+		 * 
+		 */
 		create: {
 			params: {
-				index: "string",
-				type: "string",
-				id: "string",
-				body: { name: "string" }
+				newProduct: {
+					index: "string",
+					type: "string",
+					title: "string"
+				}
 			},
 			handler(ctx) {
 				return new this.Promise((resolve, reject) => {
 					ctx.call("es.create", {
-						index: ctx.params.index,
-						type: ctx.params.type,
-						id: ctx.params.id,
-						body: { name: ctx.params.body.name }
+						index: ctx.params.newProduct.index,
+						type: ctx.params.newProduct.type,
+						title: ctx.params.newProduct.title
 					}).then(res => {
 						resolve({ success: true });
 					}).catch((err) => {
@@ -28,6 +42,19 @@ module.exports = {
 				})
 			}
 		},
+		/**
+		 * @api {post} /prodcuts/query query on indexed products
+		 * @apiName query products
+		 * @apiGroup products
+		 *
+		 * @apiParam  {String} index product index.
+		 * @apiParam  {String} q query string.
+		 *
+		 * @apiSuccess {Boolean} success ensuring success
+		 * @apiSuccess {Object} res elasticsearch response with results found.
+
+		 * @apiError {Boolean}  success ensuring failure to found results.
+		 */
 		query: {
 			params: {
 				index: "string",
@@ -41,7 +68,10 @@ module.exports = {
 					}).then(res => {
 						resolve({ success: true, res: res });
 					}).catch((err) => {
-						reject({ success: false, err: err });
+						reject({ 
+							success: false, 
+							// err: err 
+						});
 					});
 				});
 			}
