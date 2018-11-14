6c76dd95c8af9bd08b5d04a84b50e9377695ebe7
bisho <bishoymelekwadie@gmail.com>
2018-11-14 14:54:52 +0200

    CartService/changed parameters,added docs,added structured error res

--- services/products.service.js
+++ services/products.service.js
@@ -7,74 +7,98 @@ module.exports = {
 	dependencies: [],
 	actions: {
 		/**
-		 * @api {post} /prodcuts/create Add(index) new product
+		 * @api {post} /products/create Add(index) new product
 		 * @apiName create products
 		 * @apiGroup products
 		 *
+		 * @apiHeader (MyHeaderGroup) {String} authorization Authorization value.
+		 * 
 		 * @apiParam (newProduct) {Object} newProduct new product object to index.
 		 * @apiParam (newProduct) {String} newProduct.index product index.
+		 * @apiParam (newProduct) {String} newProduct.id product id.
 		 * @apiParam (newProduct) {String} newProduct.type  product type.
 		 * @apiParam (newProduct) {String} newProduct.title product title.
 		 *
 		 * @apiSuccess {Boolean} success ensuring indexing
-		 * @apiError {Boolean}  success ensuring failure to index.
 		 * 
+		 * @apiError (Error) {Boolean}  success ensuring that operation failed.
+		 * @apiError (Error) {String}  [err] error object.
+		 * @apiError (Error) {String}  [msg] error helpful message.
+		 *
+		 * @apiError (Error_params) {String}  [paramsValidation] error helpful message.
 		 */
-		create: {
+		createProd: {
 			params: {
 				newProduct: {
-					index: "string",
-					type: "string",
-					title: "string"
+					type: "object",
+					props: {
+						index: "string",
+						id: "string",
+						type: "string",
+						title: "string"
+					}
 				}
 			},
 			handler(ctx) {
-				return new this.Promise((resolve, reject) => {
-					ctx.call("es.create", {
-						index: ctx.params.newProduct.index,
-						type: ctx.params.newProduct.type,
-						title: ctx.params.newProduct.title
-					}).then(res => {
-						resolve({ success: true });
-					}).catch((err) => {
-						reject({ success: false, err: err });
-					})
+				let newProduct = ctx.params.newProduct;
+				return ctx.call("es.create", {
+					index: newProduct.index,
+					id: newProduct.id,
+					type: newProduct.type,
+					body: {
+						title: newProduct.title
+					}
 				})
+					.then(() => ({ success: true }))
+					.catch(err => this.errorRes("failed to create new product", err));
 			}
 		},
 		/**
-		 * @api {post} /prodcuts/query query on indexed products
+		 * @api {post} /products/query query on indexed products in elasticsearch
 		 * @apiName query products
 		 * @apiGroup products
 		 *
-		 * @apiParam  {String} index product index.
-		 * @apiParam  {String} q query string.
+    	 * @apiHeader (MyHeaderGroup) {String} authorization Authorization value.
+		 * 
+		 * @apiParam  {Object} query query object to look for.
+		 * @apiParam  {String} query.index product index.
+		 * @apiParam  {String} query.q query string.
 		 *
 		 * @apiSuccess {Boolean} success ensuring success
-		 * @apiSuccess {Object} res elasticsearch response with results found.
-
+		 * @apiSuccess {Object} result elasticsearch response with results found.
+		 * 
 		 * @apiError {Boolean}  success ensuring failure to found results.
 		 */
 		query: {
 			params: {
-				index: "string",
-				q: "string",
+				query: {
+					type: "object",
+					props: {
+						index: "string",
+						q: "string",
+					}
+				}
 			},
 			handler(ctx) {
-				return new this.Promise((resolve, reject) => {
-					ctx.call("es.search", {
-						index: ctx.params.index,
-						q: ctx.params.q
-					}).then(res => {
-						resolve({ success: true, res: res });
-					}).catch((err) => {
-						reject({ 
-							success: false, 
-							// err: err 
-						});
-					});
-				});
+				let query = ctx.params.query;
+				return ctx.call("es.search", {
+					index: query.index,
+					q: query.q
+				})
+					.then(res => ({ success: true, result: res }))
+					.catch(err => this.errorRes("failed to query", err));
 			}
+		}
+	},
+	methods: {
+		errorRes(msg, errObj) {
+			console.log(msg, errObj)
+			if (msg && errObj)
+				return { success: false, msg: msg, err: errObj }
+			else if (msg)
+				return { success: false, msg: msg };
+			else if (errObj)
+				return { success: false, err: errObj };
 		}
 	}
 };
