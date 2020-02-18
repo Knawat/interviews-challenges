bad3cbae096c800b654bf4189ce1a0b01fa195b9
Ahmed Sami <ahmed.sami@robustastudio.com>
2020-02-18 21:32:46 +0200

    feat: Cleaned up App.vue and added Navigation
    Removed unncesssary views, routes, components and assets.
    Added a new Navigation component for navbar

---
+++ frontend-challenge-sami/src/components/Navigation.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+header
+    nav
+    ul
+        li
+            router-link(to="/") Knawat Shopping
+        li
+            button Cart
+</template>
+
+<script>
+export default {
+  name: "Navigation"
+};
+</script>
--- frontend-challenge-sami/src/views/Home.vue
+++ frontend-challenge-sami/src/views/Home.vue
@@ -1,13 +1,8 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+<template lang="pug">
+
 </template>
 
 <script>
-// @ is an alias to /src
-
 export default {
   name: "Home",
   components: {}
