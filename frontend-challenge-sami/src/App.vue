6abf8d2859a40250602a3752a731c042d98a1621
Ahmed Sami <ahmed.sami@robustastudio.com>
2020-02-18 23:33:36 +0200

    feat: authorizing app on mounting
    Set up a proxy server for all API requests.
    Set up a network request to fetch token from the API and save it to local storage.

--- frontend-challenge-sami/src/App.vue
+++ frontend-challenge-sami/src/App.vue
@@ -12,6 +12,30 @@ export default {
   name: "App",
   components: {
     Navigation
+  },
+  mounted() {
+    if (!localStorage.getItem("token")) this.authorize();
+  },
+  methods: {
+    async authorize() {
+      const response = await fetch("/api/token", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          consumerKey: process.env.VUE_APP_CONSUMER_KEY,
+          consumerSecret: process.env.VUE_APP_CONSUMER_SECRET
+        })
+      });
+      const data = await response.json();
+
+      const {
+        channel: { token }
+      } = data || { channel: { token: null } };
+
+      localStorage.setItem("token", token);
+    }
   }
 };
 </script>
