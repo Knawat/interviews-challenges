e5040440b57e12b9b3a8e59e170010d86a83dc72
Ahmed Sami <ahmed.sami@robustastudio.com>
2020-02-18 23:42:21 +0200

    feat: fetching products list
    Added a network request to fetch products catalog from the API and iterate over them in ProductsList component

--- frontend-challenge-sami/src/components/ProductsList.vue
+++ frontend-challenge-sami/src/components/ProductsList.vue
@@ -1,7 +1,8 @@
 <template lang="pug">
 section.products-list
-  article.products-list__product(v-for="n in 10")
-    h2 {{'product ' + n}}
+  article.products-list__product(v-for="product in products" :key="product.sku")
+    h2 {{'product ' + product.sku}}
+
   pagination
 </template>
 
@@ -12,6 +13,29 @@ export default {
   name: "ProductsList",
   components: {
     Pagination
+  },
+  data: () => ({
+    products: [],
+    total: 0
+  }),
+  mounted() {
+    this.getProducts();
+  },
+  methods: {
+    async getProducts() {
+      const response = await fetch("/api/catalog/products", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`
+        }
+      });
+
+      const data = await response.json();
+      const { products, total } = data || { products: [], total: 0 };
+
+      this.products = products;
+      this.total = total;
+    }
   }
 };
 </script>
