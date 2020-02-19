de12c8d0ea651260ca899a6a6e8dd809d36c9558
Ahmed Sami <ahmed.sami@robustastudio.com>
2020-02-19 13:34:43 +0200

    feat: header styles
    Updated header styling and added a color palette to stylus variables.

--- frontend-challenge-sami/src/components/Navigation.vue
+++ frontend-challenge-sami/src/components/Navigation.vue
@@ -1,17 +1,22 @@
 <template lang="pug">
-header
-  nav
-    ul
-      li
-        router-link(to="/") Knawat Shopping
-      li
-        button Cart
-        p(v-if="$store.getters.cartCount") {{$store.getters.cartCount}}
-        ul
-          li(v-for="item in $store.getters.cartItems")
-            p {{item.product.name.en}} : {{item.quantity}}
-            button(@click="$store.commit('REMOVE_CART_ITEM', item.product.sku)") Remove
-        router-link(to="/cart") View Cart
+header.header
+  .container
+    nav
+      ul.nav-menu
+        li
+          router-link.nav-menu__logo(to="/") Knawat Shop
+        li
+          button.nav-menu__cart
+            svg(style='width:24px;height:24px', viewBox='0 0 24 24')
+              path(fill="#393535" d="M17,18C15.89,18 15,18.89 15,20A2,2 0 0,0 17,22A2,2 0 0,0 19,20C19,18.89 18.1,18 17,18M1,2V4H3L6.6,11.59L5.24,14.04C5.09,14.32 5,14.65 5,15A2,2 0 0,0 7,17H19V15H7.42A0.25,0.25 0 0,1 7.17,14.75C7.17,14.7 7.18,14.66 7.2,14.63L8.1,13H15.55C16.3,13 16.96,12.58 17.3,11.97L20.88,5.5C20.95,5.34 21,5.17 21,5A1,1 0 0,0 20,4H5.21L4.27,2M7,18C5.89,18 5,18.89 5,20A2,2 0 0,0 7,22A2,2 0 0,0 9,20C9,18.89 8.1,18 7,18Z")
+            p.nav-menu__cart-count 3
+          
+          p(v-if="$store.getters.cartCount") {{$store.getters.cartCount}}
+          ul
+            li(v-for="item in $store.getters.cartItems")
+              p {{item.product.name.en}} : {{item.quantity}}
+              button(@click="$store.commit('REMOVE_CART_ITEM', item.product.sku)") Remove
+          //- router-link(to="/cart") View Cart
 </template>
 
 <script>
@@ -19,3 +24,33 @@ export default {
   name: "Navigation"
 };
 </script>
+
+<style lang="stylus" scoped>
+.header
+  background $grey
+  padding: 1.2rem 0
+
+.nav-menu
+  display flex
+  justify-content space-between
+
+  &__logo
+    font-size 1.4rem
+    font-weight bold
+    color $dim-grey
+
+  &__cart
+    position relative
+
+  &__cart-count
+    height 1rem
+    width 1rem
+    border-radius 50%
+    background $orange
+    position absolute
+    right -6px
+    top -6px
+    display flex
+    justify-content center
+    align-items center
+</style>
